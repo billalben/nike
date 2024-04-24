53f3836300bfa478a049a582007e616b4155adba
Benzazoua <billalben252@gmail.com>
2024-04-24 20:53:54 +0100

    Popular Products Section

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
 
-export { Nav, ShoeCard, Button };
+export { Nav, ShoeCard, Button, PopularProductCard };
--- src/constants/index.js
+++ src/constants/index.js
@@ -24,7 +24,7 @@ import {
 
 export const navLinks = [
   { href: "#home", label: "Home" },
-  { href: "#about-us", label: "About Us" },
+  { href: "#products", label: "Products" },
   { href: "#services", label: "Services" },
   { href: "#subscribe", label: "Subscribe" },
 ];
