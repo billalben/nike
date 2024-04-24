8f781d6b3bf4eb446bc77c68aeb6fe1f6c5c02a7
Benzazoua <billalben252@gmail.com>
2024-04-24 18:48:58 +0100

    Navbar section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { useState } from "react";
+import { hamburgerOpen, hamburgerClose } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
 
-export default Nav
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
+        </a>
+        <ul className="flex-1 hidden md:flex justify-center items-center gap-16">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray hover:text-website-orange transition duration-300 ease-in-out cursor-pointer"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+        <div onClick={handleNav} className="block md:hidden">
+          <img
+            src={nav ? hamburgerClose : hamburgerOpen}
+            alt="hamburger icon"
+            width={25}
+            height={25}
+          />
+        </div>
+
+        {/* Mobile Navigation Menu */}
+        <nav
+          className={
+            nav
+              ? "fixed md:hidden left-0 top-0 w-[60%] h-full bg-[#000300] ease-in-out duration-500"
+              : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+          }
+        >
+          <figure className="py-8">
+            <img
+              src={headerLogo}
+              alt="logo"
+              width={129}
+              height={29}
+              className="w-[129px] h-[29px] m-auto"
+            />
+          </figure>
+
+          <ul>
+            {/* Mobile Navigation Items */}
+            {navLinks.map((item) => (
+              <li key={item.href}>
+                <a
+                  href={item.href}
+                  className="block font-montserrat p-4 border-b border-gray-600 leading-normal text-lg text-slate-gray hover:text-website-orange transition duration-300 ease-in-out cursor-pointer"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ export default {
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
         "white-400": "rgba(255, 255, 255, 0.80)",
+        "website-orange": "#ff6452",
       },
       boxShadow: {
         "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
